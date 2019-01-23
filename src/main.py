35c750dfa3e5dd18e85e555c217fe9e769e2fac9
we <david@wemakeprice.com>
2019-01-23 16:09:38 +0900

    practice 1d conv for pytorch

--- src/main.py
+++ src/main.py
@@ -1,13 +1,23 @@
 import torch
 import src.mnistModel as model
+import torch.nn as nn
 
 net = model.MNISTConvNet()
 print(net)
 
 
-input = torch.randn(1, 1, 28, 28)
+input = torch.randn(1, 28, 28)
 
-print(input.shape)
+# add 1 dim to 0 dim
 
 out = net(input)
-print(out.size())
+print("out.shape :", out.shape)
+
+
+target = torch.tensor([3], dtype=torch.long)
+print("target", target.shape)
+loss_fn = nn.CrossEntropyLoss()
+err = loss_fn(out, target)
+err.backward()
+
+print(err)
